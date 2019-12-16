2af8451c73fd6b458e1e864ffa1dd22ddb6f263b
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-17 03:33:25 +0400

    Added basic about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div id="about" class="text-center">
+  <h1 class="display-3 font-weight-bold mb2">
+    About
+
+  </h1>
+
+  <p class="display-1 w-50-l w-60-md w-90 auto">
+    <a href="https://www.vecteezy.com/free-vector/space" target="_blank">Space Vectors by Vecteezy</a>
+    <br>
+    Created by <a href="https://zethleonardo.netlify.com" target="_blank">Zeth Leonardo</a>
+  </p>
+</div>
 </template>
