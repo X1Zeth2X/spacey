3b96e3d1356294b1c1a7321b3a3a36446b2b0104
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-17 03:48:06 +0400

    Reasons..

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,6 +6,8 @@
   </h1>
 
   <p class="display-1 w-50-l w-60-md w-90 auto">
+    This is for a cool friend of mine!
+    <br>
     <a href="https://www.vecteezy.com/free-vector/space" target="_blank">Space Vectors by Vecteezy</a>
     <br>
     Created by <a href="https://zethleonardo.netlify.com" target="_blank">Zeth Leonardo</a>
