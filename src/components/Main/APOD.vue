c374f878e10507b30f997a85f5a468a7e4d62089
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-17 09:05:16 +0400

    Use proper API data and Vuex.

--- src/components/Main/APOD.vue
+++ src/components/Main/APOD.vue
@@ -7,9 +7,9 @@
           <v-icon class="mr2">mdi-weather-night</v-icon>
           Astronomy Picture of the Day
         </v-card-title>
-        <a href="https://apod.nasa.gov/apod/image/1912/M77Bfield_NASA_1200.jpg" target="_blank">
+        <a :href="apod.hd_url" target="_blank">
         <v-img
-          src="https://apod.nasa.gov/apod/image/1912/M77Bfield_NASA_960.jpg"
+          :src="apod.url"
         ></v-img>
 
         </a>
@@ -23,33 +23,20 @@
           Details
         </v-card-title>
 
-      <v-card-text>
-        <span class="white--text">The Magnetic Fields of Spiral Galaxy M77:</span>
-        <br>
-        "Can magnetic fields help tell us how spiral galaxies form and evolve?
-        To find out, the HAWC+ instrument on NASA's airborne (747)
-        SOFIA observatory observed nearby spiral galaxy M77.
-        HAWC+ maps magnetism by observing polarized infrared light emitted by
-        elongated dust grains rotating in alignment with the local magnetic field.
-        The HAWC+ image shows that magnetic fields do appear to trace
-        the spiral arms in the inner regions of M77,
-        arms that likely highlight density waves in the inflowing gas,
-        dust and stars caused by the gravity of the galaxy's oval shape.
-        The featured picture superposes the HAWC+ image over diffuse X-ray emission
-        mapped by NASA's NuSTAR satellite and visible light images taken by Hubble and the SDSS.
-        M77 is located about 47 million light years away toward the constellation
-        of the Sea Monster (Cetus)."
-      </v-card-text>
+        <v-card-text>
+          <span class="white--text">{{ apod.title }}:</span>
+          <br>
+          "{{ apod.explanation }}"
+        </v-card-text>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-card-title class="font-weight-bold">
-        <v-icon class="mr2">mdi-calendar</v-icon>
-        Published on 2019-12-16
-      </v-card-title>
+        <v-card-title class="font-weight-bold">
+          <v-icon class="mr2">mdi-calendar</v-icon>
+          Published on {{ apod.date }}
+        </v-card-title>
       </v-card>
     </v-col>
-
   </v-row>
 
 </div>
@@ -58,7 +45,12 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Getter } from 'vuex-class';
+
+const namespace: string = 'apod';
 
 @Component
-export default class APOD extends Vue {}
+export default class APOD extends Vue {
+  @Getter('apod', { namespace }) public apod: any;
+}
 </script>
