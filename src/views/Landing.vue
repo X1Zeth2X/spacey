299a5fd3de05daa5aac73a4bdd4b3e817133c82c
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-17 04:34:23 +0400

    Decoupled APICard.

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -8,31 +8,7 @@
     A cool web app about space and everything related.
   </p>
 
-  <v-card
-    max-width="35rem"
-    class="auto b-card mt-6 w-90"
-    color="#341270"
-    elevation="10"
-  >
-    <v-card-text>
-      <p>
-        To get the full experience please enter your NASA Open API Key.
-        Which can be acquired <a href="https://api.nasa.gov/" target="_blank">here</a>.
-      </p>
-
-      <v-text-field
-        outlined
-        label="NASA OpenAPI Key"
-      >
-      </v-text-field>
-
-      <v-btn block rounded large class="teal font-weight-black">
-        <v-icon>mdi-rocket</v-icon>
-        Lift Off
-      </v-btn>
-    </v-card-text>
-  </v-card>
-
+  <APICard class="mt-6"/>
 </div>
 </template>
 
@@ -40,6 +16,12 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 
-@Component
+import APICard from '@/components/Landing/APICard.vue';
+
+@Component({
+  components: {
+    APICard,
+  },
+})
 export default class Landing extends Vue {}
 </script>
