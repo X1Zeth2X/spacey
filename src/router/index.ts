efd68f46c17cb54e5bcaae2eaa233b1bebeeb08d
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-17 04:28:43 +0400

    Added navigation guards.

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,17 +9,35 @@ const routes = [
     path: '/',
     name: 'home',
     component: Landing,
+    meta: {
+      public: true,
+      onlyWhenLoggedOut: true,
+    },
   },
   {
     path: '/about',
     name: 'about',
     component: () => import('@/views/About.vue'),
+    meta: {
+      public: true,
+      onlyWhenLoggedOut: false,
+    },
   },
   {
     path: '/facts',
     name: 'facts',
     component: () => import('@/views/Facts.vue'),
+    meta: {
+      public: true,
+      onlyWhenLoggedOut: false,
+    },
   },
+  {
+    path: '/main',
+    name: 'main',
+    component: () => import('@/views/Main.vue'),
+  },
+
 ];
 
 const router = new VueRouter({
@@ -28,11 +46,24 @@ const router = new VueRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
   const isPublic = to.matched.some(record => record.meta.public);
+  // User is considered logged in if they have a key.
+  const onlyWhenLoggedOut = to.matched.some(record => record.meta.onlyWhenLoggedOut);
+
   const hasKey = !!localStorage.getItem('apiKey');
 
-  next();
+  if (!isPublic && !hasKey) {
+    return next({
+      name: 'home',
+    });
+  }
+
+  if (hasKey && onlyWhenLoggedOut) {
+    return next({ name: 'main' });
+  }
+
+  return next();
 });
 
 export default router;
