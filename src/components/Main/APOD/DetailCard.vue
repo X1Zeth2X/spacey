2f7cc1f8501e3e5bf39f1c0fe8174a9c29899c2f
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-20 07:32:00 +0400

    Decoupled APOD.

---
+++ src/components/Main/APOD/DetailCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card class="b-card" color="#835961" elevation="10">
+    <v-card-title class="font-weight-bold">
+    <v-icon class="mr2">mdi-telescope</v-icon>
+      Details
+    </v-card-title>
+
+    <v-card-text>
+      <span class="white--text">{{ title }}:</span>
+      <br>
+      "{{ explanation }}"
+    </v-card-text>
+
+    <v-divider></v-divider>
+
+    <v-card-title class="font-weight-bold">
+      <v-icon class="mr2">mdi-calendar</v-icon>
+      Published on {{ date }}
+    </v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Component
+export default class DetailCard extends Vue {
+  @Prop() public title: any;
+
+  @Prop() public explanation: any;
+
+  @Prop() public date: any;
+}
+</script>
---
+++ src/components/Main/APOD/ImageCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="b-card" elevation="10" color="#835961">
+    <v-card-title class="font-weight-bold">
+      <v-icon class="mr2">mdi-weather-night</v-icon>
+      Astronomy Picture of the Day
+    </v-card-title>
+    <a :href="hdurl" target="_blank">
+      <v-img
+        :src="url"
+        :lazy-src="url"
+      >
+        <template v-slot:placeholder>
+          <v-skeleton-loader
+            tile
+            transition="scale-transition"
+            type="image"
+            class="h-100"
+          ></v-skeleton-loader>
+        </template>
+      </v-img>
+    </a>
+
+    <v-card-text v-show="!!copyright">
+      Copyright: "{{ copyright }}"
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+
+@Component
+export default class ImageCard extends Vue {
+  @Prop() public url: any;
+
+  @Prop() public hdurl: any;
+
+  @Prop() public copyright: any;
+}
+</script>
