917e9a0fb124f1269e004773a111a191fd2fa0c6
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-20 07:32:50 +0400

    View component that manages current main view.

---
+++ src/components/Main/ViewController.vue
@@ -0,0 +1,27 @@
+<template>
+<div id="viewController">
+  <v-text-field
+    filled
+    label="Please enter a command:"
+    placeholder="user@spacey:~$ "
+  >
+    <template v-slot:append-outer>
+      <v-btn icon class="info mr2" elevation="5">
+        <v-icon>mdi-help</v-icon>
+      </v-btn>
+
+      <v-btn icon class="warning" elevation="5">
+        <v-icon>mdi-camera-control</v-icon>
+      </v-btn>
+    </template>
+  </v-text-field>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class ViewController extends Vue {}
+</script>
