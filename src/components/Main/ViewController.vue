4534d132b4e4d931baf8984f00c2adddd8962a1f
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-21 06:18:22 +0400

    Added help dialog togglers.

--- src/components/Main/ViewController.vue
+++ src/components/Main/ViewController.vue
@@ -8,7 +8,12 @@
     @keyup.enter="runCommand"
   >
     <template v-slot:append-outer>
-      <v-btn icon class="info mr2" elevation="5">
+      <v-btn
+        icon
+        class="info mr2"
+        elevation="5"
+        @click="openMainHelp"
+      >
         <v-icon>mdi-help</v-icon>
       </v-btn>
 
@@ -23,9 +28,14 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import { Action } from 'vuex-class';
+
+const namespace: string = 'dialog';
 
 @Component
 export default class ViewController extends Vue {
+  @Action('openMainHelp', { namespace }) public openMainHelp: any;
+
   public command: string = '';
 
   public runCommand() {
