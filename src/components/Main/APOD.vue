7121650c6a3a2ac453c03d9ca3bf474b0bad5b96
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-22 06:30:11 +0400

    Added loading animations and readiness.

--- src/components/Main/APOD.vue
+++ src/components/Main/APOD.vue
@@ -1,6 +1,21 @@
 <template>
 <div id="apod">
-  <v-row align="start" justify="space-between">
+  <div class="text-center mt4"
+    v-if="!ready"
+  >
+    <v-progress-circular
+      class="text-center"
+      indeterminate
+      size="50"
+      width="10"
+      :color="load.color"
+    ></v-progress-circular>
+    <p class="font-weight-black subheading mt2">
+      {{ load.msg }}
+    </p>
+  </div>
+
+  <v-row align="start" justify="space-between" v-else>
     <v-col cols="12" xs="12" md="6">
       <ImageCard
         :hdurl="apod.hdurl"
@@ -22,12 +37,17 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
-import { Getter } from 'vuex-class';
+import { Getter, Action } from 'vuex-class';
 
 import ImageCard from './APOD/ImageCard.vue';
 import DetailCard from './APOD/DetailCard.vue';
 
 const namespace: string = 'apod';
+
+interface Loading {
+  msg: string,
+  color: string,
+}
 
 @Component({
   components: {
@@ -36,6 +56,39 @@ const namespace: string = 'apod';
   },
 })
 export default class APOD extends Vue {
+  @Action('getAPOD', { namespace }) public getAPOD: any;
+
   @Getter('apod', { namespace }) public apod: any;
+
+  public ready: boolean = false;
+
+  public load: Loading = {
+    msg: 'Loading awesomeness...',
+    color: 'purple',
+  }
+
+  public created() {
+    if (!this.ready) {
+      this.loadAPOD();
+    }
+  }
+
+  public async loadAPOD() {
+    const info = await this.getAPOD();
+
+    if (info === false) {
+      this.load.msg = 'Invalid request, check API key.';
+      this.load.color = 'error';
+
+      localStorage.removeItem('apiKey');
+      setTimeout(() => {
+        this.$router.push({
+          name: 'home',
+        });
+      }, 3 * 1000);
+    } else {
+      this.ready = true;
+    }
+  }
 }
 </script>
