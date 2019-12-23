af6fad87f0cf0d31c4f03d1cc3c769c3b7bc61f4
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-23 08:11:53 +0400

    Added APODImage HD image viewer.

---
+++ src/components/Dialogs/APODImage.vue
@@ -0,0 +1,45 @@
+<template>
+<v-dialog
+  :value.sync="show"
+  persistent
+  width="785"
+>
+  <v-card class="b-card">
+    <v-img
+      contain
+      v-if="type === 'image'"
+      :src="url"
+    >
+    <template>
+      <v-row align="start" justify="end">
+        <v-btn icon class="teal mr3 mt2"
+          @click="toggleHDImage"
+        >
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-row>
+    </template>
+    </v-img>
+  </v-card>
+</v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import { Getter, Action } from 'vuex-class';
+
+const namespace: string = 'dialog';
+
+@Component
+export default class APODImage extends Vue {
+  @Prop() url!: string;
+
+  @Prop() type!: string;
+
+  @Getter('hdImage', { namespace }) public show!: boolean;
+
+  @Action('toggleHDImage', { namespace }) public toggleHDImage!: Function;
+}
+</script>
