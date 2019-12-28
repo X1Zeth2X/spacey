b56ca9f57919e2a729018de5bbb854fecb9a97aa
Zeth Leonardo <zethiscool11@gmail.com>
2019-12-28 16:45:23 +0400

    Moved verse, and added placeholder.

--- src/components/Dialogs/IVLInfo/InfoImage.vue
+++ src/components/Dialogs/IVLInfo/InfoImage.vue
@@ -1,35 +1,41 @@
 <template>
 <div>
-<v-img
-  :src="mainSrc"
-  :lazy-src="lazySrc"
-  class="br4 elevation-5"
->
-  <v-row align="start" justify="space-between">
-    <v-btn
-      class="mt2 deep-purple"
-      @click="close"
-    >
-      <v-icon
-      >mdi-chevron-left</v-icon>
-      Back
-    </v-btn>
+  <v-img
+    :src="assets !== null ? assets[1].href : ''"
+    :lazy-src="lazySrc"
+    class="br4 elevation-5"
+  >
+    <template v-slot:placeholder>
+      <v-row align="center" justify="center"
+        class="fill-height b"
+      >Loading.
+      </v-row>
+    </template>
 
-    <a :href="mainSrc" download target="_blank">
+    <v-row align="start" justify="space-between">
+      <v-btn
+        class="mt2 deep-purple"
+        @click="close"
+      >
+        <v-icon
+        >mdi-chevron-left</v-icon>
+        Back
+      </v-btn>
+
+      <a v-if="assets !== null"
+        :href="assets[0].href"
+        download
+        target="_blank"
+      >
+
       <v-btn
         icon
         class="mr3 mt2"
-        @click="downloadImage"
       ><v-icon>mdi-download</v-icon>
       </v-btn>
     </a>
-  </v-row>
-</v-img>
-
-  <v-alert color="deep-purple" elevation="2" class="mt2">
-    "Heaven and earth will pass away, but my words will not pass away."
-    - <strong>Matthew 24:35</strong>
-  </v-alert>
+    </v-row>
+  </v-img>
 
   <v-alert
     v-show="type === 'video'"
@@ -41,6 +47,11 @@
     Video files isn't supported as of now,
     I apologize for the inconvenience :(
   </v-alert>
+
+  <v-alert color="deep-purple" elevation="2" class="mt2">
+    "Heaven and earth will pass away, but my words will not pass away."
+    - <strong>Matthew 24:35</strong>
+  </v-alert>
 </div>
 </template>
 
@@ -48,13 +59,13 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import { Prop } from 'vue-property-decorator';
-import { Action } from 'vuex-class';
+import { Action, Getter } from 'vuex-class';
 
 @Component
 export default class InfoImage extends Vue {
   @Action('toggleIVLInfo', { namespace: 'dialog' }) public close!: Function;
 
-  @Prop() public mainSrc!: string;
+  @Getter('assetsInfo', { namespace: 'ivl' }) public assets!: object[];
 
   @Prop() public lazySrc!: string;
 
